5622acfaf6f0775fe5e0682e0f5f16c389fedff5
Kate-Kitkat19 <103567509+Kate-Kitkat19@users.noreply.github.com>
2022-07-19 10:39:10 +0000

    Deploying to gh-pages from @ Kate-Kitkat19/Ice-cream_project@4d6176aef1911360c7e0a0774cd7712926c5f131 🚀

--- index.4a11cb8e.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0}});
-//# sourceMappingURL=index.4a11cb8e.js.map
--- index.dcea3ded.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0}});
-//# sourceMappingURL=index.dcea3ded.js.map
